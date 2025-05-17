ff1d71767ed8317fdbc65937365770c5b2c426fa
Olga Gol <olga.golovatskaia@gmail.com>
2025-05-17 17:57:50 -0400

    test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,13 +8,13 @@ const HomePage = () => (
       </div>
       <div className="introduction-section">
       <h1>Hey, I'm <strong>Olga (Olya) Golovatskaia</strong></h1>
-      <p>Im a Computer Science and Mathematics student at Mount Holyoke College with a strong foundation in
-         <strong>algorithms</strong>, <strong>machine learning</strong>, <strong>optimization</strong>, and <strong>computational geometry</strong>.
+      <p>I'm a Computer Science and Mathematics student at Mount Holyoke College with a strong foundation in
+        <strong> algorithms</strong>, <strong>machine learning</strong>, <strong>optimization</strong>, and <strong>computational geometry</strong>.
         I enjoy coding, solving complex problems, and working on collaborative projects that bridge theory and real-world applications.
       </p>
       <p>I work with languages like <strong>Java</strong>, <strong>JavaScript</strong>, and <strong>Python</strong>, and
         build with tools and frameworks such as <strong>React</strong>, <strong>Next.js</strong>, <strong>Node.js</strong>,
-         <strong>PyTorch</strong>, and <strong>TensorFlow</strong>.  I'm driven by challenges that call for precise, thoughtful, and scalable solutions.
+         <strong> PyTorch</strong>, and <strong>TensorFlow</strong>.  I'm driven by challenges that call for precise, thoughtful, and scalable solutions.
       </p>
       </div>
       </div>
